f026fcd29b92214c087654dd3f4fd5e6d288070d
ARAGOZ-MTS <ragozinski@gmail.com>
2020-12-24 12:28:56 +0300

    leha init

--- java_mts_brest/src/by/mts/brest/java/our_classes/EmployeeProdazhi.java
+++
@@ -1,7 +0,0 @@
-package by.mts.brest.java.our_classes;
-
-public class EmployeeProdazhi {
-    String name;
-    int vozrast;
-
-}
---
+++ src/by/mts/brest/java/LehaClass.java
@@ -0,0 +1,5 @@
+package by.mts.brest.java;
+
+public class LehaClass {
+    // my test class
+}
