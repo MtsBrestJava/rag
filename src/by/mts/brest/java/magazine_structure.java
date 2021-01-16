72e0376c30587dbaea76a574560bd6f4d81c9f30
ARAGOZ-MTS <ragozinski@gmail.com>
2021-01-16 13:28:14 +0300

    вырубился модуль

--- src/by/mts/brest/java/magazine_structure.java
+++ src/by/mts/brest/java/magazine_structure.java
@@ -1,33 +1,32 @@
 package by.mts.brest.java;
 
-    public class magazine_structure {
+public class magazine_structure {
 
-        int price;
-        int rom;
-        int ram;
-        int screen;
-        String color;
-        String name;
+    int price;
+    int rom;
+    int ram;
+    int screen;
+    String color;
+    String name;
 
-        public magazine_structure(String name, String color, int price){
-            this.name = name;
-            this.color = color;
-            this.price = price;
-            this.rom=0;
-            this.ram=0;
-            this.screen=0;
-
-        }
-
-        public magazine_structure(String name, String color, int price, int rom, int ram, int screen) {
-            this.name = name;
-            this.color = color;
-            this.rom = rom;
-            this.ram = ram;
-            this.screen = screen;
-            this.price = price;
-
-        }
+    public magazine_structure(String name, String color, int price){
+        this.name = name;
+        this.color = color;
+        this.price = price;
+        this.rom=0;
+        this.ram=0;
+        this.screen=0;
 
     }
 
+    public magazine_structure(String name, String color, int price, int rom, int ram, int screen) {
+        this.name = name;
+        this.color = color;
+        this.rom = rom;
+        this.ram = ram;
+        this.screen = screen;
+        this.price = price;
+
+    }
+
+}
