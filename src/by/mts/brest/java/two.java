aa349de23893d30a6e7c5acdf98dbb3af0cea3dc
ARAGOZ-MTS <ragozinski@gmail.com>
2021-02-06 10:53:00 +0300

    домашка 2 переменные

--- src/by/mts/brest/java/two.java
+++ src/by/mts/brest/java/two.java
@@ -1,36 +1,147 @@
 package by.mts.brest.java;
-import java.util.ArrayList;
+
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
 
 public class two {
-    ArrayList<Integer> someInts = new ArrayList<Integer>();
-    public static int a;
+    static int symbol;
+    static int pozicion = 10;
+    static int speed = 50;
+    static int s = 5;
+    static int p = 5;
 
-    public static void main(String[] args) throws InterruptedException {
+    public static void main(String[] args) {
 
-        Thread one = new Thread(new Runnable() {
-            @Override
-            public void run() {
-                for(int i = 0; i < 100; i++) {
-                    System.out.println("one -1: "+i);
-                }
-            }
-        });
+        inKeis readKeys = new inKeis();
+        readKeys.start();
 
-        one.start();
-        System.out.println("one runned");
+        Stopper stopper = new Stopper();
+        stopper.start();
+        Gogo gogo = new Gogo();
+        gogo.run();
 
-        Thread second = new Thread(new Runnable() {
-            @Override
-            public void run() {
-                for(int i = 0; i < 156; i++) {
-                    System.out.println("second ##2: "+i);
-                }
-            }
-        });
-        second.start();
 
-        System.out.println("second runned");
-        System.out.println("End of program");
+
     }
 
+
+    static class Gogo implements Runnable {
+
+        @Override
+        public void run() {
+            while (symbol != 1) {
+
+                System.out.printf("%5d", speed);
+                System.out.print("  |");
+                int i = 0;
+                while (i < pozicion) {
+                    System.out.print(" ");
+                    i++;
+                }
+                System.out.print("O");
+                int j = 0;
+                while (j < 22 - pozicion) {
+                    System.out.print(" ");
+                    j++;
+                }
+                System.out.print("| \n");
+                try {
+                    Thread.sleep(100);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+            System.out.println("----------FINICH----------");
+        }
+    }
+
+    static class inKeis extends Thread {
+
+        @Override
+        public void run() {
+
+            while (symbol != 1) {
+                p = 1 + (int) (Math.random() * 2);
+                s = 1 + (int) (Math.random() * 2);
+                try {
+                    Thread.sleep(100 * speed);
+                } catch (InterruptedException r) {
+                    r.printStackTrace();
+                }
+                switch (s) {
+
+                    case 1 -> {
+                        if (speed > 1 && speed < 99) speed += 10;
+                    }
+                    case 2 -> {
+                        if (speed > 1 && speed < 99) speed -= 10;
+                    }
+                }
+                switch (p) {
+                    case 1 -> {
+                        if (pozicion > 0 && pozicion < 19) pozicion += 2;
+                    }
+                    case 2 -> {
+                        if (pozicion > 0 && pozicion < 19) pozicion -= 2;
+                    }
+                }
+            }
+        }
+    }
+
+    static class Stopper extends Thread implements KeyListener {
+        //  Scanner in = new Scanner(System.in);
+
+        @Override
+        public void start() {
+//          while (symbol != 1) {
+//              try {
+//                  symbol = in.nextInt();
+//                  in.nextLine();
+            System.out.println(symbol);
+//              } catch (Exception e) {
+//                  e.printStackTrace();
+//              }
+            for (symbol = 100; symbol > 0; symbol--) {
+                try {
+                    Thread.sleep(2);
+                } catch (InterruptedException r) {
+                    r.printStackTrace();
+                }
+                symbol = 1;
+            }
+        }
+        @Override
+        public void keyTyped(KeyEvent e) {
+            System.out.println("jbgljkbgflajkjkjk");
+            while (e.getKeyChar() == 'a')
+//            switch (e.getKeyChar()) {
+//                case 'd':
+                symbol = 1;
+            System.out.println("STOP");
+//                    break;
+//            }
+        }
+        @Override
+        public void keyPressed(KeyEvent e) {
+            switch (e.getKeyChar()) {
+                case 'd':
+                    symbol = 1;
+                    System.out.println("STOP");
+                    break;
+            }
+        }
+
+        @Override
+        public void keyReleased(KeyEvent e){
+            switch (e.getKeyChar()) {
+                case 'd':
+                    symbol = 1;
+                    System.out.println("STOP");
+                    break;
+            }
+//            in.close();
+        }
+    }
 }
